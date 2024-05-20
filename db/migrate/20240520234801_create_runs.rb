5125a3dd8d5b20ab46bd492f6ffd24d9db94440a
Alex Wu <hehahohee@gmail.com>
2024-05-20 16:59:34 -0700

    create run model

---
+++ db/migrate/20240520234801_create_runs.rb
@@ -0,0 +1,13 @@
+class CreateRuns < ActiveRecord::Migration[7.1]
+  def change
+    create_table :runs do |t|
+      t.string :name
+      t.datetime :close_time
+      t.string :status
+      t.belongs_to :user
+
+      t.timestamps
+    end
+    add_foreign_key :runs, :users
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_20_223802) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_20_234801) do
+  create_table "runs", force: :cascade do |t|
+    t.string "name"
+    t.datetime "close_time"
+    t.string "status"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_runs_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -37,4 +47,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_20_223802) do
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
+  add_foreign_key "runs", "users"
 end
