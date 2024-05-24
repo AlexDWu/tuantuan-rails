199555f8996ad27a6cff41f577b686dd9e2fa64c
Alex Wu <hehahohee@gmail.com>
2024-05-24 15:23:14 -0700

    create order model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_24_184937) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_24_205119) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -46,6 +46,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_24_184937) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["run_id"], name: "index_items_on_run_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "run_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["run_id"], name: "index_orders_on_run_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "runs", force: :cascade do |t|
@@ -87,5 +96,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_24_184937) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "items", "runs"
+  add_foreign_key "orders", "runs"
+  add_foreign_key "orders", "users"
   add_foreign_key "runs", "users"
 end
